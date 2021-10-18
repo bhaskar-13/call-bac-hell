e9deaee75c09481e5a2120f5beba4d7792bd4598
bhaskar-13 <53254229+bhaskar-13@users.noreply.github.com>
2021-10-18 18:11:43 +0530

    Update promise.js

--- promise.js
+++ promise.js
@@ -1,3 +1,4 @@
+// 98% we dont create promise but use the promise
 const msg=(count)=>new Promise((resolve,reject)=>{
     
     if(count>=1)
@@ -14,7 +15,7 @@ const msg=(count)=>new Promise((resolve,reject)=>{
             document.querySelector(".countdown").innerText="Happy Independence day";
             reject("completed");
         },1000);
-         
+         //calling callback function
     }
 });
 
@@ -27,5 +28,23 @@ msg(9).then((ct)=>msg(--ct))//8
       .then((ct)=>msg(--ct))//2
       .then((ct)=>msg(--ct))//1
       .then((ct)=>msg(--ct))//Happy Independence day
-      .catch((msg)=>console.log(msg));
+      .catch((msg)=>console.log(msg));//In the above program we have created a function message which accepets a parameter and takes in promise object which intend taes two call back.In the resolve the settime out is used in which the header is selected and the count value is assigned only after 1 second.If the condition fails it goes to reject stated to print happy indepence day and message completed. then use .then to chain it and hence catch the error using .catch.
 
+
+
+//promises takes in a function while creating which itself takes two call back function.
+
+// states in promies are 1.Pending 2.Fullfilled 3.Rejected.
+// console.log(ilegal);
+
+// ilegal.then(msg=>msg + "congragulation")
+//        .then((msg1)=>console.log(msg1+"Heaven"))//channeing process
+//       .catch((msg)=>console.log(msg));
+
+// const data=JSON.parse('{"x":4,"name":"bhaskar"}');
+// console.log(data);
+
+// setTimeout(()=>{
+//     document.querySelector(".countdown").innerText=count;
+
+// },1000);
